2a2b47146b6e7e4dc596983705255bebc7b1857d
NoHomey <ivo.stratev.tues@gmail.com>
2020-10-28 18:30:53 +0200

    test2

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "962f4824f5efcf9c4b9d";
+/******/ 	var hotCurrentHash = "9eb7da6898d16d9e1d44";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
