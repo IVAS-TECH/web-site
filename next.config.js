833d72ba3bc2deb6c89ca945fdc61f397e5741d5
NoHomey <ivo.stratev.tues@gmail.com>
2020-10-29 13:50:58 +0200

    ignoring less plugin for now (problem with serving the SSG)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const less = require('@zeit/next-less');
-const withPlugins = require('next-compose-plugins');
-
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withPlugins([
-    [less, { lessLoaderOptions: { javascriptEnabled: true } }]
-], isProd ? { basePath: '/web-site' } : {});
+module.exports = isProd ? { basePath: '/web-site', assetPrefix: '/web-site' } : {};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
-import '../styles/theme.less';
+import '../styles/rsuite.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
