cd9624234c00f999006bd9a5d5d176645fa35f77
NoHomey <ivo.stratev.tues@gmail.com>
2020-11-02 19:09:06 +0200

    using rem instead of px in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,51 +11,56 @@ const Home = () => (
         </Head>
         <Layout>
             <Panel shaded style={{
-                width: 640,
-                marginTop: 60,
-                marginLeft: 400
+                width: '40rem',
+                marginTop: '4rem',
+                marginLeft: '22rem'
             }}>
                 <img width="600" height="200" src="https://cdn-media.itead.cc/media/wysiwyg/PCB/SMTStencil_banner.jpg" />
-                <p style={{ marginTop: 20 }}>
-                    <b>Ivas Tech</b> manifactures SMT stencils and produces thin metal elements such as buisness cards.
+                <p style={{ marginTop: '1rem' }}>
+                    <b>Ivas Tech</b> manifactures SMT stencils and produces thin and precise metal elements such as buisness cards.
                 </p>
             </Panel>
             <Panel shaded style={{
-                width: 700,
-                marginTop: 60,
-                marginLeft: 500
+                width: '42rem',
+                marginTop: '4rem',
+                marginLeft: '30rem'
             }}>
                 You can read more about SMT stencils, types of SMT stencils, fiducial marks,
                 nano coating and electrochemical polishing at the <NavLink href="/about">About page</NavLink>.
             </Panel>
             <Panel shaded style={{
-                width: 860,
-                marginTop: 50,
-                marginLeft: 450
+                width: '55rem',
+                marginTop: '4rem',
+                marginLeft: '25rem'
             }}>
                 At the <NavLink href="/we-offer">We offer page</NavLink> you can find all the details about what <NavLink href="/we-offer">SMT stencils we can manifacture</NavLink> and find information about <NavLink href="/we-offer/other">our capbilities for producing thin metal elements</NavLink>.
             </Panel>
             <Panel shaded style={{
-                width: 800,
-                marginTop: 60,
-                marginLeft: 370
+                width: '50rem',
+                marginTop: '4rem',
+                marginLeft: '23rem'
             }}>
                 As the name suggests at the <NavLink href="/equipment">Equipment page</NavLink> you can find all about our equipment.
             </Panel>
             <Panel shaded style={{
-                width: 1000,
-                marginTop: 70,
-                marginLeft: 250
+                width: '57rem',
+                marginTop: '4rem',
+                marginLeft: '20rem'
             }}>
-                By visiting <a>Order</a> you will be redirected to our WEB application for ordering SMT stencils.
-                If you want us to produce thin metal elements for you or have any questions please <NavLink href="/contact">Contact us</NavLink>.
-                We also have a <NavLink href="/faq">FAQ</NavLink> page where you can find answers to frequantly asked questions.
-                
+                <p>
+                    By visiting <a>Order</a> you will be redirected to our WEB application for ordering SMT stencils.
+                </p>
+                <p>
+                    If you want us to produce thin metal elements for you or have any questions please <NavLink href="/contact">Contact us</NavLink>.
+                </p>
+                <p>
+                    We also have a <NavLink href="/faq">FAQ</NavLink> page where you can find answers to frequantly asked questions.
+                </p>
             </Panel>
             <Panel shaded style={{
-                width: 600,
-                marginTop: 60,
-                marginLeft: 300
+                width: '37rem',
+                marginTop: '4rem',
+                marginLeft: '26rem'
             }}>
                 In our <NavLink href="/blog">Blog</NavLink> we post information about SMT stencils, companies wich trust us, news about our company and more.
             </Panel>
