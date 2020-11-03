4309957acf2795299284816c49712ba45f89e570
NoHomey <ivo.stratev.tues@gmail.com>
2020-11-03 18:56:59 +0200

    adding test design pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f73c573d9fd56e2a3e66";
+/******/ 	var hotCurrentHash = "b4c1b122e381cac4a336";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
