721326c597bafe1adc3c80540bd5605d7d304171
NoHomey <ivo.stratev.tues@gmail.com>
2020-11-12 17:23:18 +0200

    new version

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3c23391963ccd6f04977";
+/******/ 	var hotCurrentHash = "419411fff0c322332d5e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
